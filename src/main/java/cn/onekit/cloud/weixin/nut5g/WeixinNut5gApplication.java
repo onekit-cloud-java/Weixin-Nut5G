86301fcfe6a9e9724fc04c9acd3b6e23c7ecd49c
your name <your email>
2021-03-05 14:10:47 +0800

    .

---
+++ src/main/java/cn/onekit/cloud/weixin/nut5g/WeixinNut5gApplication.java
@@ -0,0 +1,13 @@
+package cn.onekit.cloud.weixin.nut5g;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinNut5gApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeixinNut5gApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/onekit/cloud/weixin/nut5g/WeixinNut5gApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.onekit.cloud.weixin.nut5g;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeixinNut5gApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
